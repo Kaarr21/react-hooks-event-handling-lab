2c2ceef275867d9badaa8c6175d7230d568279aa
Karoki <wanjikukar19@gmail.com>
2025-04-11 13:45:57 +0300

    Initial

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input type="password" placeholder="Enter your code" onChange={handleChange} />
+    </div>
+  );
 }
 
 export default Keypad;
